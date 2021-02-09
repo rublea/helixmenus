7dc372f2624b941e4f9b5a35fa595165c3574c03
rublea <56970363+rublea@users.noreply.github.com>
2021-02-09 12:50:07 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -41,7 +41,7 @@ function wrap(selector, className) {
 function createHeroSection() {
   const $headerImg = document.querySelector('main>div:first-of-type>div>:first-child>img');
   const path = window.location.pathname;
-  path.includes('lunch') ? $wrapper.style.backgroundImage = `url("/hlx_badfd8f4207119f1e7ad796dd3d89ec51f09951c.jpeg")` : console.log('not yet');
+  path.includes('lunch') ? $wrapper.style.backgroundImage = url("/hlx_badfd8f4207119f1e7ad796dd3d89ec51f09951c.jpeg") : console.log('not yet');
   if ($headerImg) {
     const src = $headerImg.getAttribute('src');
     const $wrapper = $headerImg.closest('.section-wrapper');
